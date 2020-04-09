ffbb71636713d25fef028d24cfaf275cd1e9e238
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-04-09 18:22:39 -0400

    update webpack to support decorators

--- packages/file-daemon/webpack.config.js
+++ packages/file-daemon/webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
               "@babel/plugin-transform-typescript",
               "@babel/plugin-proposal-optional-chaining",
               "@babel/plugin-proposal-nullish-coalescing-operator",
+              ["@babel/plugin-proposal-decorators", { legacy: true }],
               "@babel/plugin-proposal-class-properties",
             ],
           },
