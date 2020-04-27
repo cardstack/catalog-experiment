81ce7bfc9f11577cf6fcc1fefa5a254854336d25
Edward Faulkner <ef@alum.mit.edu>
2020-04-27 17:08:53 -0400

    re-enable test

--- packages/builder/test/index.ts
+++ packages/builder/test/index.ts
@@ -5,7 +5,7 @@ import "./path-test";
 import "./builder-test";
 import "./builder-nodes-test";
 import "./filesystem-test";
-//import "./file-daemon-client-test";
+import "./file-daemon-client-test";
 import "./builder-acceptance-test";
 
 QUnit.start();
