5283b88f46fed51f67a1f5e0826d869f31a0d698
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-05-01 09:42:37 -0400

    moving things around

--- packages/builder/test/combine-modules-test.ts
+++ packages/builder/test/combine-modules-test.ts
@@ -13,47 +13,45 @@ import { FileSystem } from "../src/filesystem";
 
 let resolver = new Resolver(); // TODO need to resolve modules without '.js' extension
 
+function bundleAtUrl(assignments: BundleAssignments, url: URL): Bundle {
+  return assignments.bundles.find((b) => b.url.href === url.href)!;
+}
+
+async function makeModuleResolutions(
+  fs: FileSystem,
+  moduleURL: URL
+): Promise<ModuleResolution> {
+  let js = await (await fs.open(moduleURL)).readText();
+  let parsed = parse(js);
+  if (parsed?.type !== "File") {
+    throw new Error(`parsed js for ${moduleURL.href} is not a babel File type`);
+  }
+  let imports: ModuleResolution["imports"] = {};
+  await Promise.all(
+    describeImports(parsed).map(async (desc) => {
+      let depURL = await resolver.resolve(desc.specifier, moduleURL);
+      imports[desc.specifier] = {
+        desc,
+        resolution: await makeModuleResolutions(fs, depURL),
+      };
+    })
+  );
+  return { url: moduleURL, parsed, imports };
+}
+
+async function makeBundleAssignments(
+  fs: FileSystem,
+  entrypoints: URL[],
+  config?: AssignmentConfig
+): Promise<BundleAssignments> {
+  let resolutions = await Promise.all(
+    entrypoints.map((e) => makeModuleResolutions(fs, e))
+  );
+  return new BundleAssignments(resolutions, config);
+}
+
 QUnit.module("combine modules", function (origHooks) {
   let { test } = installFileAssertions(origHooks);
-
-  function bundleAtUrl(assignments: BundleAssignments, url: URL): Bundle {
-    return assignments.bundles.find((b) => b.url.href === url.href)!;
-  }
-
-  async function makeModuleResolutions(
-    fs: FileSystem,
-    moduleURL: URL
-  ): Promise<ModuleResolution> {
-    let js = await (await fs.open(moduleURL)).readText();
-    let parsed = parse(js);
-    if (parsed?.type !== "File") {
-      throw new Error(
-        `parsed js for ${moduleURL.href} is not a babel File type`
-      );
-    }
-    let imports: ModuleResolution["imports"] = {};
-    await Promise.all(
-      describeImports(parsed).map(async (desc) => {
-        let depURL = await resolver.resolve(desc.specifier, moduleURL);
-        imports[desc.specifier] = {
-          desc,
-          resolution: await makeModuleResolutions(fs, depURL),
-        };
-      })
-    );
-    return { url: moduleURL, parsed, imports };
-  }
-
-  async function makeBundleAssignments(
-    fs: FileSystem,
-    entrypoints: URL[],
-    config?: AssignmentConfig
-  ): Promise<BundleAssignments> {
-    let resolutions = await Promise.all(
-      entrypoints.map((e) => makeModuleResolutions(fs, e))
-    );
-    return new BundleAssignments(resolutions, config);
-  }
 
   test("it can combine modules", async function (assert) {
     await assert.setupFiles({
