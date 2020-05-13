2b8f252ee35cb7356c5aea86ef4daeddd4828430
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-05-13 10:48:32 -0400

    Adding support for running node tests from a TSC build via ESM because my lapotop is going _very_ slow when debugging the webpack bundle (sadly esm doesn't play nice with ES2020 which supports nullish colascing ಥʖ̯ಥ

---
+++ packages/builder-node/test/test-entrypoint.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+const esmRequire = require("esm")(module, { cjs: true });
+module.exports = esmRequire("./index");
