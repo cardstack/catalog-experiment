b99ced28effe4e41c995550865c01ec83f4a177f
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-05-19 15:06:02 -0400

    fixed bug around cross origin requests being forwarded correctly

--- packages/builder-worker/src/request-handlers/file-request-handler.ts
+++ packages/builder-worker/src/request-handlers/file-request-handler.ts
@@ -21,17 +21,15 @@ export const handleFileRequest: Handler = async function (req, context) {
   // For the webpack hosted builder requests, we need to honor same origin
   // policy (apparently), as the responses are empty otherwise. So instead route
   // these requests through the file daemon where they will be proxied to the
-  // webpack hosted builder. I wonder about 3rd party js, we might need the file
-  // daemon to be more aggressive with its proxying....
+  // webpack hosted builder.
   if (requestURL.origin === builderOrigin) {
     return new Response(
       (await fetch(new URL(requestURL.pathname, originURL).href)).body
     );
   }
 
-  // This might not be doing what we think it's doing per the comment above...
   if (!originURL.href || requestURL.origin !== originURL.origin) {
-    return new Response((await fetch(req)).body);
+    return await fetch(req);
   }
 
   console.log(`serving request ${requestURL} from filesystem`);
