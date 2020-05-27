84a93e1265926af267ef317cee67188eba2b47ff
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-05-27 10:29:40 -0400

    using absolute path for image, as there is no guarantee on which route this element is rendered

--- packages/builder-node/bin/build.ts
+++ packages/builder-node/bin/build.ts
@@ -1,3 +1,5 @@
+#! node
+
 //@ts-ignore not actually redefining block-scoped var, the esmRequire in bin/build.ts  is not used in the testing context
 const esmRequire = require("esm")(module, { cjs: true });
 esmRequire("../src/build");
