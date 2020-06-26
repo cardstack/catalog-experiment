32610c2d5bf482ff16dda947440cdfd9d62423f3
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-06-26 09:33:36 -0400

    whitespace tweak

--- packages/builder-worker/test/builder-test.ts
+++ packages/builder-worker/test/builder-test.ts
@@ -195,9 +195,7 @@ QUnit.module("module builder", function (origHooks) {
           import { puppies } from "./puppies.js";
 
           function getPuppies() { return puppies; }
-
           function getCats() { return ["jojo"]; }
-
           function getRats() { return ["pizza rat"]; }
 
           export { getPuppies, getCats, getRats };
