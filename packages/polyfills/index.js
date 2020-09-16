4d3c5994a909ee9377c1eef3ef774b49d4ee948c
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-09-16 18:16:58 -0400

    adding path builtin node polyfill

--- packages/polyfills/index.js
+++ packages/polyfills/index.js
@@ -2,3 +2,4 @@ export { default as assert } from "./assert";
 export { default as buffer } from "./buffer";
 export { default as process } from "./process";
 export { default as util } from "./util";
+export { default as path } from "./path";
