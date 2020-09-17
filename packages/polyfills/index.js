e6662fd7f0bf5813556ac67c7541ab955bd9342a
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2020-09-17 12:59:36 -0400

    more polyfills

--- packages/polyfills/index.js
+++ packages/polyfills/index.js
@@ -3,3 +3,7 @@ export { default as buffer } from "./buffer";
 export { default as process } from "./process";
 export { default as util } from "./util";
 export { default as path } from "./path";
+export { default as url } from "./url";
+export { default as querystring } from "./querystring";
+export { default as punycode } from "./punycode";
+export { default as fs } from "./fs-stub";
