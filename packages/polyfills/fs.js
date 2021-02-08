9ac5d7dd8cabd30bb673d494bd957ed07465324a
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-02-08 15:56:59 -0500

    CS-176 high fidelity mock of node fs

--- packages/polyfills/fs.js
+++ packages/polyfills/fs.js
@@ -1,6 +1,26 @@
 // This is intended to just throw if any of the node fs API functions are
 // evaluated. For an actual working node fs polyfill, refer to the polyfill that
 // rollup and browserify use: https://github.com/mafintosh/browserify-fs
-export default {};
-
-// TODO export named API functions that throw...
+function throwWhenCalled() {
+  throw new Error(`node 'fs' module is not available in the browser context`);
+}
+export default {
+  readFileSync: throwWhenCalled,
+  readFile: throwWhenCalled,
+  readdirSync: throwWhenCalled,
+  readdir: throwWhenCalled,
+  readlinkSync: throwWhenCalled,
+  readlink: throwWhenCalled,
+  read: throwWhenCalled,
+  exists: throwWhenCalled,
+  existsSync: throwWhenCalled,
+  write: throwWhenCalled,
+  writeFile: throwWhenCalled,
+  writeFileSync: throwWhenCalled,
+  stat: throwWhenCalled,
+  statSync: throwWhenCalled,
+  open: throwWhenCalled,
+  openSync: throwWhenCalled,
+  opendir: throwWhenCalled,
+  opendirSync: throwWhenCalled,
+};
